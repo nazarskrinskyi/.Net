a39ff038c05e08110122f2701ed3361a9e88ac21
Nazar <90456305+nazarskrinskyi@users.noreply.github.com>
2024-03-03 15:58:05 +0200

    add lab 1

---
+++ lab1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Program
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.Write("Good, Morning!");
+            Console.ReadLine();
+        } 
+    }
+}
